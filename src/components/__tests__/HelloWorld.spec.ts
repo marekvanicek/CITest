1bb1735672037ca61efa92e4a1ba8441516e60df
Marek Vaníček <marekvanicek.9@gmail.com>
2023-02-23 00:09:36 +0100

    fix: unit test

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -8,8 +8,4 @@ describe('HelloWorld', () => {
         const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
         expect(wrapper.text()).toContain('Hello Vitest')
     })
-    it('test failingg :/', () => {
-        const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-        expect(wrapper.text()).toContain('2 + 2 is four brew')
-    })
 })
