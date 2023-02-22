d55767de27a24e80130c39f06a66dcaca895f1f2
Marek Vaníček <marekvanicek.9@gmail.com>
2023-02-23 00:07:00 +0100

    fix: lint

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,6 @@ import TheWelcome from './components/TheWelcome.vue'
 const heybro = (num?: number) => {
     return num + 22;
 }
-
-const bro = () => {
-    console.log('dasjkdasjkdsahjkasdhjkasdhjkdashjkadskhjadhjsadsjkhjkdashjkasdhjkasdjkadsjkadshjkasdhjkadhjsadhjskadjskkadhjsadhjskhjkasdkadhjsk')
-}
-
-
 </script>
 
 <template>
