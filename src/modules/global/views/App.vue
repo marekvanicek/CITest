58704b56d83003ba4dc0fb8be5c26a36bfc76eed
Marek Vaníček <marekvanicek.9@gmail.com>
2023-02-25 20:26:26 +0100

    chores: restructure

--- src/modules/global/views/App.vue
+++ src/modules/global/views/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import PostContent from '@/modules/posts/components/PostContent.vue';
 </script>
 
 <template>
     <div class="bg-red-300 p-4">
-        god
+        hello world :)
+        <PostContent/>
     </div>
 </template>
 
---
+++ src/modules/posts/tests/Post.test.ts
@@ -0,0 +1,9 @@
+import {describe, expect, test} from 'vitest'
+
+describe('Post', () => {
+    test('Test', () => {
+        const foo = 4
+        expect(foo).toEqual(4)
+    })
+})
+
